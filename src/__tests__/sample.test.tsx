3937656e108b67d0196ef9011c86a59605a1b653
hkdream880 <hkdream880@gmail.com>
2020-10-24 17:02:50 +0900

    add test env

---
+++ src/__tests__/sample.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+describe('test for test', () => {
+  test('should display a blank login form, with remember me checked by default', async () => {
+    console.log('test')
+  })
+})
---
+++ src/globals.d.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/react/dont-cleanup-after-each'
